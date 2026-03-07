24082879554ce25132ca28154836570115fbe12d
AllenD <allengdiaz2019@gmail.com>
2026-03-07 19:05:09 +0800

    test: TestFlux - first step for fux for my reactive programming learning

--- src/test/java/com/demo/webflux/MonoFlusTest.java
+++ src/test/java/com/demo/webflux/MonoFlusTest.java
@@ -1,12 +1,27 @@
 package com.demo.webflux;
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public class MonoFlusTest {
 
     @Test
     public void testMono() {
-        Mono<String> monoString = Mono.just("javatechie");
-        monoString.subscribe(System.out::println);
+        Mono<?> monoString = Mono.just("javatechie")
+                .then(Mono.error(new RuntimeException("Exception Occurred")))
+                .log();
+                
+        monoString.subscribe(System.out::println, (e)-> System.out.println(e.getMessage()));
     }
+
+    @Test
+    public void testFlux() {
+        Flux<String> fluxString = Flux.just("Spring", "Spring Boot", "Hibernate", "Microservices")
+                .concatWithValues("Aws")
+                .concatWith(Flux.error(new RuntimeException("Exception Occurred in Flux")))
+                .log();
+            
+
+        fluxString.subscribe(System.out::println, (e)-> System.out.println(e.getMessage()));  
+    }       
 }
